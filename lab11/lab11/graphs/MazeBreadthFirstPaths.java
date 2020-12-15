95a5888e5553eb12fb0bfaa49eee5b78b3ff3210
lambdafate <lambdafate@163.com>
2020-12-15 11:27:41 +0800

    update

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,7 +1,10 @@
 package lab11.graphs;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeBreadthFirstPaths extends MazeExplorer {
     /* Inherits public fields:
@@ -9,21 +12,48 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private Maze maze;
+    private int s;
+    private int t;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
-    /** Conducts a breadth first search of the maze starting at the source. */
+    /**
+     * Conducts a breadth first search of the maze starting at the source.
+     */
     private void bfs() {
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(s);
+        while (!queue.isEmpty()) {
+            int v = queue.poll();
+            marked[v] = true;
+            announce();
+            if (v == t) {
+                break;
+            }
+            for (int tmp : maze.adj(v)) {
+                if (distTo[tmp] == Integer.MAX_VALUE) {
+                    edgeTo[tmp] = v;
+                    distTo[tmp] = distTo[v] + 1;
+                    queue.offer(tmp);
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,7 +1,7 @@
 package lab11.graphs;
 
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeCycles extends MazeExplorer {
     /* Inherits public fields:
@@ -9,16 +9,47 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private boolean cycle;
 
     public MazeCycles(Maze m) {
         super(m);
+        cycle = false;
     }
 
     @Override
     public void solve() {
         // TODO: Your code here!
+        boolean[] visited = new boolean[marked.length];
+        cycleCheck(63, visited);
     }
 
+
     // Helper methods go here
+    private void cycleCheck(int v, boolean[] visited) {
+        visited[v] = true;
+        // marked[v] = true;
+        // announce();
+        for (int w : maze.adj(v)) {
+            if (!visited[w]) {
+                edgeTo[w] = v;
+                cycleCheck(w, visited);
+                if (cycle) {
+                    break;
+                }
+            } else if (w != edgeTo[v]) {
+                cycle = true;
+                edgeTo[w] = v;
+                while (v != w) {
+                    marked[v] = true;
+                    announce();
+                    v = edgeTo[v];
+                }
+                marked[w] = true;
+                announce();
+                break;
+            }
+        }
+
+    }
 }
 
